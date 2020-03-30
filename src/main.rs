a64b0d1bd155c603280ee30d821212de652f0f22
S4ltyGo4t <mario.weidner@gmx.de>
2020-03-30 09:36:41 +0200

    added tdd example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+pub fn say(s: &str) -> String {
+  let r = String::from("hello ");
+  return r + s;
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  
+  #[test]
+  fn say_hello() {
+    let result = say("ssvm");
+    assert!(result.contains("hello ssvm"));
+  }
 }
